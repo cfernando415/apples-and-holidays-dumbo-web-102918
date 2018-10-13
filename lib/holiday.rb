5fac8b6d0d3e8a3f676a15c5a3a2970774ba560a
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-13 00:16:01 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,6 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season}:"
     holidays.each do |holiday, supplies|
       puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
+    end
     
 end
 
